b40c996ab1e2384d8653b9e3f84b681ec617a402
mccarthykeelan <mccarthykeelan@gmail.com>
2026-06-13 16:52:23 +0100

    Update generateQR functionality to display error

--- projects/QrCodeGenerator/script.js
+++ projects/QrCodeGenerator/script.js
@@ -6,8 +6,15 @@ const qrText = document.getElementById("qrText");
 const btnGenerate = document.getElementById("btnGenerate");
 
 const generateQR = function () {
-  qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
-  imgBox.classList.add("show-img");
+  if (qrText.value.length > 0) {
+    qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+    imgBox.classList.add("show-img");
+  } else {
+    qrText.classList.add("error");
+    setTimeout(() => {
+      qrText.classList.remove("error");
+    }, 1000);
+  }
 };
 
 btnGenerate.addEventListener("click", generateQR);
