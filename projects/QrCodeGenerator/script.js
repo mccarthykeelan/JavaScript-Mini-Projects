fd01557cf58e05e131cdc18e786eb882e8f48c47
mccarthykeelan <mccarthykeelan@gmail.com>
2026-06-13 16:46:59 +0100

    add elements, event listeners and implement generateQR function

--- projects/QrCodeGenerator/script.js
+++ projects/QrCodeGenerator/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const imgBox = document.getElementById("imgBox");
+const qrImage = document.getElementById("qrImage");
+const qrText = document.getElementById("qrText");
+const btnGenerate = document.getElementById("btnGenerate");
+
+const generateQR = function () {
+  qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+  imgBox.classList.add("show-img");
+};
+
+btnGenerate.addEventListener("click", generateQR);
