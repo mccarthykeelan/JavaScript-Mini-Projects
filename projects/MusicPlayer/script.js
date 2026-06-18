fc425514af24e5aa601bf4b9702b64b6a266c28e
Keelan McCarthy <mccarthykeelan@gmail.com>
2026-06-18 02:18:03 +0100

    add javascript functionality

--- projects/MusicPlayer/script.js
+++ projects/MusicPlayer/script.js
@@ -1,1 +1,37 @@
 "use strict";
+
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+
+const playPause = function () {
+  if (ctrlIcon.classList.contains("fa-pause")) {
+    song.pause();
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+  } else {
+    ctrlIcon.classList.remove("fa-play");
+    ctrlIcon.classList.add("fa-pause");
+    song.play();
+  }
+};
+
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+
+progress.onchange = function (e) {
+  song.play();
+  song.currentTime = progress.value;
+  ctrlIcon.classList.remove("fa-play");
+  ctrlIcon.classList.add("fa-pause");
+};
+
+ctrlIcon.addEventListener("click", playPause);
