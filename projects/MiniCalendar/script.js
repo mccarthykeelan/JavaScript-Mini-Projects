48b3b3d9037473d9362479aab84ba47580acb3fd
Keelan McCarthy <mccarthykeelan@gmail.com>
2026-06-23 17:36:11 +0100

    Add javascript for date functionality

--- projects/MiniCalendar/script.js
+++ projects/MiniCalendar/script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+const currentDate = new Date();
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+date.textContent = currentDate.getDate();
+
+day.textContent = days[currentDate.getDay()];
+
+month.textContent = months[currentDate.getMonth()];
+
+year.textContent = currentDate.getFullYear();
