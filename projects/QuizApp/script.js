60ff0f8309c520b6bb18d8002b9c433e34f655a9
Keelan McCarthy <mccarthykeelan@gmail.com>
2026-06-26 23:36:57 +0100

    add quiz questions and answers to script.js

--- projects/QuizApp/script.js
+++ projects/QuizApp/script.js
@@ -1,1 +1,88 @@
 "use strict";
+
+const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      {
+        text: "Shark",
+        correct: false,
+      },
+      {
+        text: "Blue Whale",
+        correct: true,
+      },
+      {
+        text: "Elephant",
+        correct: false,
+      },
+      {
+        text: "Giraffe",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "What is the smallest country in the world?",
+    answers: [
+      {
+        text: "Vatican City",
+        correct: true,
+      },
+      {
+        text: "Bhutan",
+        correct: false,
+      },
+      {
+        text: "Nepal",
+        correct: false,
+      },
+      {
+        text: "Shri Lanka",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "What is the largest desert in the world?",
+    answers: [
+      {
+        text: "Kalahari",
+        correct: false,
+      },
+      {
+        text: "Gobi",
+        correct: false,
+      },
+      {
+        text: "Sahara",
+        correct: false,
+      },
+      {
+        text: "Antarctica",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "Which is the smallest continent in the world?",
+    answers: [
+      {
+        text: "Asia",
+        correct: false,
+      },
+      {
+        text: "Australia",
+        correct: true,
+      },
+      {
+        text: "Arctic",
+        correct: false,
+      },
+      {
+        text: "Africa",
+        correct: false,
+      },
+    ],
+  },
+];
