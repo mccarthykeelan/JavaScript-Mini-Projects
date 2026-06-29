cdfa6f0e86b0b9909a3c77fca99bb0c3986c82d5
Keelan McCarthy <mccarthykeelan@gmail.com>
2026-06-29 22:58:12 +0100

    update createBtn functionality to avoid focus bug

--- projects/NotesApp/script.js
+++ projects/NotesApp/script.js
@@ -5,10 +5,26 @@ const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
 createBtn.addEventListener("click", () => {
+  let note = document.createElement("div");
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
+
+  note.className = "note";
   inputBox.className = "input-box";
-  inputBox.setAttribute("contenteditable", "true");
+  inputBox.contentEditable = "true";
+
   img.src = "images/delete.png";
-  notesContainer.appendChild(inputBox).appendChild(img);
+  img.className = "delete";
+
+  note.appendChild(inputBox);
+  note.appendChild(img);
+  notesContainer.appendChild(note);
+
+  inputBox.focus();
+});
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+  }
 });
