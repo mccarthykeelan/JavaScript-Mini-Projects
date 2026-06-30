afbf1d11e2c5b51b2b4d16bf5340af00f943101b
Keelan McCarthy <mccarthykeelan@gmail.com>
2026-06-30 03:08:43 +0100

    Update and add event listeners, add showNotes and updateStorage functionality

--- projects/NotesApp/script.js
+++ projects/NotesApp/script.js
@@ -4,6 +4,16 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", () => {
   let note = document.createElement("div");
   let inputBox = document.createElement("p");
@@ -26,5 +36,20 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBReak");
+    event.preventDefault();
   }
 });
