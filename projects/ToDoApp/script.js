215b61c9136e972b2a5348bd7f6521be0c095d9e
Keelan McCarthy <mccarthykeelan@gmail.com>
2026-07-07 03:11:31 +0100

    add DOM pointers to inputBox, listContainer, btnAdd. Implement add task function, list container click event

--- projects/ToDoApp/script.js
+++ projects/ToDoApp/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const btnAdd = document.getElementById("btnAdd");
+
+const addTask = function () {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+};
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false,
+);
+
+btnAdd.addEventListener("click", addTask);
